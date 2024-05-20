73ea659bee45d990e769402ec7373811d57cd314
Emma Chen <emmachen@starlux-airlines.com>
2024-05-20 16:06:49 +0800

    adding form validation with zod

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,10 +1,24 @@
-'use server';
+"use server";
 
+import { z } from "zod";
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: "Must be lowercase letters or dashes without spaces",
+    }),
+  description: z.string().min(10),
+});
 export async function createTopic(formData: FormData) {
-    const name = formData.get('name');
-    const description = formData.get('description');
+  const result = createTopicSchema.safeParse({
+    name: formData.get('name'),
+    description: formData.get('description')
+  });
+  if(!result.success) {
+    console.log(result.error.flatten().fieldErrors);
+  }
 
-    console.log(name, description);
-    // TODO: revalidate the homepage
-
+  // TODO: revalidate the homepage
 }
